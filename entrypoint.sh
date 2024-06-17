db5b5ab9e0ed46b014448d62ae1045ed3171b3cc
kenkato <yosyuaomenw@yahoo.co.jp>
2024-06-17 18:46:37 +0900

    mastertomain

--- entrypoint.sh
+++ entrypoint.sh
@@ -78,7 +78,7 @@ if [ -z "$(git status --porcelain)" ]; then
 fi && \
 git add . && \
 git commit -m 'Deploy to GitHub Pages' && \
-git push --force $REMOTE_REPOSITORY master:$REMOTE_BRANCH && \
+git push --force $REMOTE_REPOSITORY main:$REMOTE_BRANCH && \
 rm -fr .git && \
 cd $GITHUB_WORKSPACE && \
 echo "Content of $BUILD_DIR has been deployed to GitHub Pages."
